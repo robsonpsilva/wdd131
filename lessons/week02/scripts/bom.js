4e64db1e0dc026bf5db1027bd3e8388a0fea0f8f
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-08-13 16:15:52 -0300

    renamed:    personal_study/lesson1/index.html -> lessons/personal_study/lesson1/index.html 	renamed:    personal_study/lesson10/index.html -> lessons/personal_study/lesson10/index.html 	renamed:    personal_study/lesson2/index.html -> lessons/personal_study/lesson2/index.html 	renamed:    personal_study/lesson2/pic_bulboff.gif -> lessons/personal_study/lesson2/pic_bulboff.gif 	renamed:    personal_study/lesson2/pic_bulbon.gif -> lessons/personal_study/lesson2/pic_bulbon.gif 	renamed:    personal_study/lesson3/index.html -> lessons/personal_study/lesson3/index.html 	renamed:    personal_study/lesson4/index.html -> lessons/personal_study/lesson4/index.html 	renamed:    personal_study/lesson5/index.html -> lessons/personal_study/lesson5/index.html 	renamed:    personal_study/lesson6/index.html -> lessons/personal_study/lesson6/index.html 	renamed:    personal_study/lesson7/index,html -> lessons/personal_study/lesson7/index,html 	renamed:    personal_study/lesson8/index.html -> lessons/personal_study/lesson8/index.html 	renamed:    personal_study/lesson9/index.html -> lessons/personal_study/lesson9/index.html 	renamed:    node-v20.16.0-win-x64.zip -> lessons/personal_study/node-v20.16.0-win-x64.zip 	renamed:    week01/basic-layout.html -> lessons/week01/basic-layout.html 	renamed:    week01/styles/layout.css -> lessons/week01/styles/layout.css 	renamed:    week02/aula01/index.html -> lessons/week02/aula01/index.html 	renamed:    week02/aula01/scripts/loop.js -> lessons/week02/aula01/scripts/loop.js 	renamed:    week02/aula02/index.html -> lessons/week02/aula02/index.html 	renamed:    week02/aula02/scripts/loops.js -> lessons/week02/aula02/scripts/loops.js 	renamed:    week02/aula02/scripts/loops1.js -> lessons/week02/aula02/scripts/loops1.js 	renamed:    week02/aula03/img/dinosaur.jpg -> lessons/week02/aula03/img/dinosaur.jpg 	renamed:    week02/aula03/index.html -> lessons/week02/aula03/index.html 	new file:   lessons/week02/bom.html 	renamed:    week02/exercise01/index01.html -> lessons/week02/exercise01/index01.html 	renamed:    week02/exercise01/scripts/learnjavascripr.js -> lessons/week02/exercise01/scripts/learnjavascripr.js 	new file:   lessons/week02/scripts/bom.js 	new file:   lessons/week02/styles/bom.css 	renamed:    week02/media-query.html -> media-query.html 	renamed:    week02/styles/media-query-large.css -> styles/media-query-large.css 	renamed:    week02/styles/media-query.css -> styles/media-query.css

---
+++ lessons/week02/scripts/bom.js
@@ -0,0 +1,27 @@
+
+const in1 = document.querySelector('#favchap');  // defining an input variable
+const addbtn = document.querySelector('button'); // defining a button variable
+const list1 = document.querySelector('#list'); //defining a list variable
+
+function apend_user_choice(){
+    
+    const text= document.createTextNode(in1.value); //Getting input entry
+    
+    if (text.length != 0){//Verifing if different of empty string
+        //If there is a value, add a li element in the ordered list
+        const bookLi = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        deleteButton.ariaLabel = 'Delete scripture line'
+        deleteButton.textContent= 'X';
+        bookLi.appendChild(text);
+        bookLi.appendChild(deleteButton);
+        list1.append(bookLi);
+        in1.value = '';
+        console.log(deleteButton.ariaLabel);
+    }
+    else{
+        alert('You need to inform a scripture book and chapter!')
+    }
+    
+}
+
