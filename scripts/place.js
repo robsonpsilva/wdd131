57086a7d17c6a64ec7b40b97d72a28ca7d5e9be9
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-09-20 06:22:26 -0300

    new file:   images/dan-cutler-l9zZMm0cBVE-unsplash.jpg 	new file:   images/elise-laine-SoWBaWALDxo-unsplash.jpg 	new file:   images/hero-rio-de-janeiro-sugarloaf-large-v1.webp 	new file:   images/hero-rio-de-janeiro-sugarloaf-medium-v2.webp 	new file:   images/hero-rio-de-janeiro-sugarloaf-medium-v2_resized.webp 	new file:   images/hero-rio-de-janeiro-sugarloaf-small-v2.webp 	new file:   images/hero-rio-de-janeiro-sugarloaf-small-v3.webp 	new file:   images/hero-rio-de-janeiro-sugarloaf.jpg 	new file:   images/pexels-lucaresi-5185881.jpg 	modified:   place.html 	modified:   scripts/place.js 	modified:   styles/place.css

--- scripts/place.js
+++ scripts/place.js
@@ -1,14 +1,32 @@
-const image = document.getElementById('heroimage');
-const div = document.getElementById('grid-container');
+const image = document.getElementById("heroimage");
+const div = document.getElementById("grid-container");
+
+const chill = document.getElementById("chill");
+
+let temperature = 20;
+let windvelocity = 20;
+let windchill = 0;
 
 // Função para ajustar a altura do outro elemento com base na altura da imagem
-function adjustheight() {
+function adjust() {
     const h = image.clientHeight; // Pegando a altura da imagem em pixels
     div.style.height = h + 'px'; // Definindo a altura do outro elemento
+    if (temperature <= 10 && windvelocity >= 4.8){
+        windchill= calculateWindChill(temperature, windvelocity);
+        chill.textContent = windchill.toFixed(1) + "°C";
+    }
+    else{
+        chill.textContent = "N/A";
+    }
 }
 
 // Ajustando a altura quando a imagem for carregada
-window.onload = adjustheight;
+window.onload = adjust;
 
 // Ajustando a altura quando a janela for redimensionada
-window.onresize = adjustheight;
+window.onresize = adjust;
+
+function calculateWindChill(temperature, windvelocity){
+    return 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windvelocity, 0.16) + 0.3965*temperature*Math.pow(windvelocity, 0.16);
+}
+
