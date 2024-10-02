cc2bb5b221fcfd0e1cbfdb138af9d0410ecc0182
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-10-02 14:59:06 -0300

    modified:   week04/lesson02/course.html 	modified:   week04/lesson02/scripts/course.js

--- week04/lesson02/scripts/course.js
+++ week04/lesson02/scripts/course.js
@@ -2,14 +2,45 @@
 const aCourse = {
     code: "CSE121b",
     name: "Javascript Language",
-    sections: [1, ['STC 353', 26, 'TTh', 'Bro T']],
+    sections:[['STC 353', 26, 'TTh', 'Bro T'], ['STC 347', 28, 'TTh', 'SIS A']],
 };
 
 setNameCode(aCourse);
+setTable(aCourse);
 
 function setNameCode(course){
     const courseName = document.getElementById('courseName');
     const courseCode = document.getElementById('courseCode');
     courseName.textContent = course.name;
     courseCode.textContent = course.code;
+}
+
+function setTable(course){
+    let tab = document.getElementById('sections');
+    let newline = tab.insertRow();
+    let cell1 = newline.insertCell(0);
+    let cell2 = newline.insertCell(1);
+    let cell3 = newline.insertCell(2);
+    let cell4 = newline.insertCell(3);
+    let cell5 = newline.insertCell(4);
+
+    cell1.textContent = '1';
+    cell2.textContent = course.sections[0][0];
+    cell3.textContent = course.sections[0][1];
+    cell4.textContent = course.sections[0][2];
+    cell5.textContent = course.sections[0][3];
+
+    newline = tab.insertRow();
+    cell1 = newline.insertCell(0);
+    cell2 = newline.insertCell(1);
+    cell3 = newline.insertCell(2);
+    cell4 = newline.insertCell(3);
+    cell5 = newline.insertCell(4);
+
+    cell1.textContent = '2';
+    cell2.textContent = course.sections[1][0];
+    cell3.textContent = course.sections[1][1];
+    cell4.textContent = course.sections[1][2];
+    cell5.textContent = course.sections[1][3];
+    
 }
