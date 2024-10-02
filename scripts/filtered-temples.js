ee59ecfd9188da2df6cd24ce533ba6f2f8b29acd
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-10-02 16:38:15 -0300

    modified:   filtered-temples.html 	modified:   scripts/filtered-temples.js 	modified:   styles/temples-large.css

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -2,6 +2,108 @@ const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 const listaItems = document.querySelectorAll('.navigation a');
 
+const temples = [
+	{
+	  templeName: "Aba Nigeria",
+	  location: "Aba, Nigeria",
+	  dedicated: "2005, August, 7",
+	  area: 11500,
+	  imageUrl:
+	  "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+	},
+	{
+	  templeName: "Manti Utah",
+	  location: "Manti, Utah, United States",
+	  dedicated: "1888, May, 21",
+	  area: 74792,
+	  imageUrl:
+	  "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+	},
+	{
+	  templeName: "Payson Utah",
+	  location: "Payson, Utah, United States",
+	  dedicated: "2015, June, 7",
+	  area: 96630,
+	  imageUrl:
+	  "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+	},
+	{
+	  templeName: "Yigo Guam",
+	  location: "Yigo, Guam",
+	  dedicated: "2020, May, 2",
+	  area: 6861,
+	  imageUrl:
+	  "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+	},
+	{
+	  templeName: "Washington D.C.",
+	  location: "Kensington, Maryland, United States",
+	  dedicated: "1974, November, 19",
+	  area: 156558,
+	  imageUrl:
+	  "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+	},
+	{
+	  templeName: "Lima Perú",
+	  location: "Lima, Perú",
+	  dedicated: "1986, January, 10",
+	  area: 9600,
+	  imageUrl:
+	  "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+	},
+	{
+	  templeName: "Mexico City Mexico",
+	  location: "Mexico City, Mexico",
+	  dedicated: "1983, December, 2",
+	  area: 116642,
+	  imageUrl:
+	  "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+	},
+	{
+		templeName: "Brazil Rio de Janeiro",
+		location: "Rio de Janeiro, Brazil",
+		dedicated: "2022, May, 8",
+		area: 38202,
+		imageUrl:
+		"https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/rio-de-janeiro-brazil/400x250/1-001db7326e638032470a02813c9e47191ef74b0e.jpeg"
+	  },
+	  {
+		templeName: "Brazil Recife",
+		location: "Recife, Brazil",
+		dedicated: "2000, December, 15",
+		area: 2400000,
+		imageUrl:
+		"https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/rio-de-janeiro-brazil/400x250/1-001db7326e638032470a02813c9e47191ef74b0e.jpeg"
+	  },
+	  {
+		templeName: "Brazil Fortaleza",
+		location: "Fortaleza, Brazil",
+		dedicated: "2009, June, 2",
+		area: 35473,
+		imageUrl:
+		"https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/fortaleza-brazil/400x250/1-Fortaleza-Temple-Photo-2233938.jpg"
+	  },
+];
+
+let m1 = document.getElementById('m_1');
+
+
+for (let i = 0; i< temples.length; i++){
+	m1.innerHTML += `
+	<div class="m1_div">
+				<div class = "inner-container">
+					<h2>Temple: ${temples[i].templeName}</h2>
+					<p>Location:${temples[i].location}</p>
+					<p>Location:${temples[i].dedicated}</p>
+					<p>Location:${temples[i].area}</p>
+					<figure class="img_container">
+						<img src="${temples[i].imageUrl}" alt="Phenix Temple" class="img_tunning">
+					</figure>
+				</div>
+    </div>
+	`
+}
+
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
