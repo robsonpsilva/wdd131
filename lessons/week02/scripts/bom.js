ecfe9334f76913852922b578c8fed65be07b1e61
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-10-15 15:47:27 -0300

    modified:   lessons/week02/scripts/bom.js

--- lessons/week02/scripts/bom.js
+++ lessons/week02/scripts/bom.js
@@ -3,7 +3,23 @@ const in1 = document.querySelector('#favchap');  // defining an input variable
 const delbtn = document.querySelector('button'); // defining a button variable
 const list1 = document.querySelector('#list'); //defining a list variable
 const btnSubmit = document.getElementById('btnSubmit');
-let index = 0;
+for (let i = 0; i < localStorage.length; i++) {
+    if (localStorage.length > 0){
+        const chave = localStorage.key(i);
+        const bookLi = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        const text = document.createTextNode(localStorage.getItem(chave));
+        deleteButton.ariaLabel = 'Delete scripture line'
+        deleteButton.textContent= 'X';
+        deleteButton.style.color = 'red';
+        bookLi.appendChild(text);
+        bookLi.appendChild(deleteButton);
+        bookLi.id = chave;
+        list1.append(bookLi);
+    }
+    
+    
+}
 
 function appendUserChoice(){
     
@@ -13,14 +29,35 @@ function appendUserChoice(){
         //If there is a value, add a li element in the ordered list
         const bookLi = document.createElement('li');
         const deleteButton = document.createElement('button');
+        const itens = document.querySelectorAll('#list li');
         deleteButton.ariaLabel = 'Delete scripture line'
         deleteButton.textContent= 'X';
         deleteButton.style.color = 'red';
         bookLi.appendChild(text);
-        localStorage.setItem(index, text);
-        index +=1;
         bookLi.appendChild(deleteButton);
-        bookLi.id = list1.length;
+        let maiorChave = -Infinity; // Inicializa com o menor valor possível
+        // Itera sobre todas as chaves no localStorage
+        for (let i = 0; i < localStorage.length; i++) {
+            const chave = localStorage.key(i);
+            
+            // Verifica se a chave é numérica
+            if (!isNaN(chave)) {
+                const chaveNumerica = Number(chave);
+                
+                // Atualiza a maior chave se a chave atual for maior
+                if (chaveNumerica > maiorChave) {
+                    maiorChave = chaveNumerica;
+                }
+            }
+        }
+        if (maiorChave == -Infinity){
+            maiorChave = 0;
+        }
+        else{
+            maiorChave++; 
+        }
+        localStorage.setItem(maiorChave, text.textContent);
+        bookLi.id = maiorChave;
         list1.append(bookLi);
         in1.value = '';
     }
@@ -31,6 +68,8 @@ function appendUserChoice(){
     
 }
 function deleteUserChoice(event){
+    localStorage.removeItem(event.target.parentElement.id);
+
     list1.removeChild(event.target.parentElement);
     in1.focus();
 }
