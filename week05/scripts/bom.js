497cd2f6a1956e99fa6be5dbf2b0337fea0be64b
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-10-15 16:31:04 -0300

    modified:   lessons/week02/scripts/bom.js 	new file:   week05/bom.html 	new file:   week05/scripts/bom.js 	new file:   week05/styles/bom.css

--- week05/scripts/bom.js
+++ week05/scripts/bom.js
@@ -3,6 +3,32 @@ const in1 = document.querySelector('#favchap');  // defining an input variable
 const delbtn = document.querySelector('button'); // defining a button variable
 const list1 = document.querySelector('#list'); //defining a list variable
 const btnSubmit = document.getElementById('btnSubmit');
+const key = 'books';
+let chaptersArray = getChapterList() || [];
+
+
+// for (let i = 0; i < localStorage.length; i++) {
+//     if (localStorage.length > 0){
+//         const chave = localStorage.key(i);
+//         const bookLi = document.createElement('li');
+//         const deleteButton = document.createElement('button');
+//         const text = document.createTextNode(localStorage.getItem(chave));
+//         deleteButton.ariaLabel = 'Delete scripture line'
+//         deleteButton.textContent= 'X';
+//         deleteButton.style.color = 'red';
+//         bookLi.appendChild(text);
+//         bookLi.appendChild(deleteButton);
+//         bookLi.id = chave;
+//         list1.append(bookLi);
+//     }
+    
+    
+// }
+
+function getChapterListchaptersArray (){
+    chaptersArray = localStorage.getItem('books');
+    return chaptersArray;
+}
 
 function appendUserChoice(){
     
@@ -17,9 +43,11 @@ function appendUserChoice(){
         deleteButton.style.color = 'red';
         bookLi.appendChild(text);
         bookLi.appendChild(deleteButton);
-        bookLi.id = list1.length;
+        bookLi.id = books.length;
+        books.push(text.textContent);
         list1.append(bookLi);
         in1.value = '';
+        localStorage.setItem(key, JSON.stringify(books) );
     }
     else{
         alert('You need to inform a scripture book and chapter!')
@@ -28,6 +56,8 @@ function appendUserChoice(){
     
 }
 function deleteUserChoice(event){
+    localStorage.removeItem(event.target.parentElement.id);
+
     list1.removeChild(event.target.parentElement);
     in1.focus();
 }
