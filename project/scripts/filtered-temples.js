60a94723ae126ba78bdafff5749e56121814ed7f
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-10-19 07:43:39 -0300

    deleted:    project/index copy.html 	modified:   project/index.html 	deleted:    project/scripts/base.js 	new file:   project/scripts/filtered-temples.js 	renamed:    project/styles/large-viewport.css -> project/styles/filtered-temples-large.css 	renamed:    project/styles/base.css -> project/styles/filtered-temples.css 	deleted:    teste.html

---
+++ project/scripts/filtered-temples.js
@@ -0,0 +1,39 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+const listaItems = document.querySelectorAll('.navigation a');
+const h2Item = document.querySelector('h2');
+
+let element_a = document.getElementsByTagName('a');
+let sel = 'Home';
+
+
+for (var i = 0; i < element_a.length; i++) {
+    element_a[i].addEventListener('click', function(event) {
+		h2Item.textContent = this.textContent;
+    });
+}
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    listaItems.forEach(item => {
+        item.addEventListener('click', () => {
+            tWayFinder(item)
+        });
+    });
+});
+
+function tWayFinder(item){
+	listaItems.forEach(elem => {
+		if (elem.textContent.trim() !== item.textContent.trim()) {
+			elem.classList.remove('active');
+		}
+		else{
+			elem.classList.add('active');
+		}
+	})
+}
