a7c31e8e571a858a9b75d5994fd59e9dd1fa3ef4
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-10-31 15:51:31 -0300

    modified:   project/contact.html 	modified:   project/contactalert.html 	new file:   project/scripts/contactalert.js 	modified:   project/styles/contact.css 	renamed:    styles/review.css -> project/styles/contactalert.css 	modified:   review.html

---
+++ project/scripts/contactalert.js
@@ -0,0 +1,17 @@
+const nreviews = document.getElementById("numberofreviews");
+let reviews = localStorage.getItem("reviewnumber");
+let sum = 0;
+if (reviews!== null){
+    sum = parseInt(reviews) + 1;
+}
+else{
+    sum = 1; 
+}
+localStorage.setItem("reviewnumber", sum);
+
+nreviews.textContent = `The number of completed reviews is: ${sum}`;
+
+function goBack() {
+    window.history.back();
+}
+
